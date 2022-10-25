9fef8777b37842907a4cbf582ae624d1076ac695
shileifu <1025174411@qq.com>
2022-10-25 17:27:28 +0800

    should aduit

--- fe_prestop.sh
+++ fe_prestop.sh
@@ -11,37 +11,31 @@ FE_LEADER=
 # host_type, default "IP"
 HOST_TYPE=${HOST_TYPE:-"IP"}
 #pod domain name for access
-POD_HOSTNAME=`hostname -f`
+POD_HOSTNAME=$(hostname -f)
 #pod ip
-POD_IP=`hostname -i`
+POD_IP=$(hostname -i)
 #help host for transfer master
 SELF_HOST=
 #left_node_lists node lists of except self.
 LEFT_NODE_LSITS=
 STARROCK_HOME=${STARROCK_HOME:-"/opt/starrocks"}
 
-
-show_frontends()
-{
+show_frontends() {
     local svc=$1
     # ensure `mysql` command can be ended with 15 seconds
     # "show frontends" query will hang when there is no leader yet in the cluster
     timeout 15 mysql --connect-timeout 2 -h $svc -P $QUERY_PORT -u root --skip-column-names --batch -e 'show frontends;'
 }
 
-log_stderr()
-{
-    echo "[`date`] $@" >&2
+log_stderr() {
+    echo "[$(date)] $@" >&2
 }
-
-
 
 #get mysql host
 # get last hosts
 # get self role
-assignment_variable()
-{
-    if [[ $HOST_TYPE == "IP" ]] ; then
+assignment_variable() {
+    if [[ $HOST_TYPE == "IP" ]]; then
         SELF_HOST=$POD_IP
     else
         SELF_HOST=$POD_HOSTNAME
@@ -49,16 +43,16 @@ assignment_variable()
 
     local svc=$1
     local memlist=
-    while true
-    do
-        memlist=`show_frontends $svc`
-        local leader=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $2}'`
+    while true; do
+        memlist=$(show_frontends $svc)
+        local leader=$(echo "$memlist" | grep '\<LEADER\>' | awk '{print $2}')
 
         if [[ "x$leader" != "x" ]]; then
-            QUERY_PORT=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $5}'`
-            EDIT_LOG_PORT=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $3}'`
-            SELF_ROLE=`echo "$memlist" | grep "\<$SELF_HOST\>" | awk '{print $6}'`
-            LEFT_NODE_LSITS=`echo "$memlist" | grep -v "\<$SELF_HOST\>" | awk '{print $1}' | xargs echo | tr ' ' ','`
+            FE_LEADER=$leader
+            QUERY_PORT=$(echo "$memlist" | grep '\<LEADER\>' | awk '{print $2}')
+            EDIT_LOG_PORT=$(echo "$memlist" | grep '\<LEADER\>' | awk '{print $5}')
+            SELF_ROLE=$(echo "$memlist" | grep "\<$SELF_HOST\>" | awk '{print $6}')
+            LEFT_NODE_LSITS=$(echo "$memlist" | grep -v "\<$SELF_HOST\>" | awk '{print $1}' | xargs echo | tr ' ' ',')
             return 0
         fi
 
@@ -67,27 +61,26 @@ assignment_variable()
     done
 }
 
-transfer_master()
-{
+transfer_master() {
 
-    `java -jar $STARROCK_HOME/fe/lib/je-7.3.7.jar DbGroupAdmin -helperHosts $SELF_HOST:$EDIT_LOG_PORT -groupName PALO_JOURNAL_GROUP -transferMaster $LEFT_NODE_LSITS 30`
+    $(java -jar $STARROCK_HOME/fe/lib/je-7.3.7.jar DbGroupAdmin -helperHosts {$SELF_HOST:$EDIT_LOG_PORT} -groupName PALO_JOURNAL_GROUP -transferMaster $LEFT_NODE_LSITS 30)
 }
 
+set_self_not_leader() {
+    while true; do
+        assignment_variable
+        if [[ "x$SELF_ROLE" == "xLEADER" ]]; then
+            transfer_master
+        fi
 
-set_self_not_leader()
-{
-    while true
-    do
-    assignment_variable
-    if [[ "x$SELF_ROLE" == "xLEADER" ]] ; then
-        transfer_master 
-    fi
-
-        local now=`date +%s`
+        local now=$(date +%s)
         let "expire=start+timeout"
-        if [[ $expire -le $now ]] ; then 
+        if [[ $expire -le $now ]]; then
+            if $has_member; then
                 log_stderr "Timed out, abort!"
                 exit 1
+            else
+                log_stderrr "Timed out, no members detected ever, assume myself is the first node .."
             fi
         fi
         sleep $PROBE_INTERVAL
@@ -95,16 +88,13 @@ set_self_not_leader()
     done
 }
 
-stop_follower_observer()
-{
-        timeout 30 mysql --connect-timeout 2 -h $FE_LEADER -P $QUERY_PORT -u root --skip-column-names --batch -e "ALTER SYSTEM DROP follower '$SELF_HOST:$EDIT_LOG_PORT';"    
-            $STARROCK_HOME/fe/bin/stop_fe.sh
+stop_follower_observer() {
+    timeout 30 mysql --connect-timeout 2 -h $FE_LEADER -P $QUERY_PORT -u root --skip-column-names --batch -e "ALTER SYSTEM DROP follower '$SELF_HOST:$EDIT_LOG_PORT';"
+    $STARROCK_HOME/fe/bin/stop_fe.sh
 }
 
-
-
 svc_name=$1
-if [[ "x$svc_name" == "x" ]] ; then
+if [[ "x$svc_name" == "x" ]]; then
     echo "Need a required parameter!"
     echo "  Example: $0 <fe_service_name>"
     exit 1
@@ -112,5 +102,4 @@ fi
 
 set_self_not_leader $svc_name
 stop_follower_observer
-
 
