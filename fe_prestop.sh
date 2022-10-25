bdb2eeac0f8b252d6e40dae72fc0567922b14c3d
shileifu <1025174411@qq.com>
2022-10-25 17:17:32 +0800

    slow complited

--- fe_prestop.sh
+++ fe_prestop.sh
@@ -41,67 +41,64 @@ log_stderr()
 # get self role
 assignment_variable()
 {
-	if [[ $HOST_TYPE == "IP" ]] ; then
-		SELF_HOST=$POD_IP
-	else
-		SELF_HOST=$POD_HOSTNAME
-	fi
+    if [[ $HOST_TYPE == "IP" ]] ; then
+        SELF_HOST=$POD_IP
+    else
+        SELF_HOST=$POD_HOSTNAME
+    fi
 
-	local svc=$1
-	local memlist=
-	while true
-	do
-		memlist=`show_frontends $svc`
-		local leader=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $2}'`
+    local svc=$1
+    local memlist=
+    while true
+    do
+        memlist=`show_frontends $svc`
+        local leader=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $2}'`
 
-		if [[ "x$leader" != "x" ]]; then
-			QUERY_PORT=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $5}'`
-			EDIT_LOG_PORT=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $3}'`
-			SELF_ROLE=`echo "$memlist" | grep "\<$SELF_HOST\>" | awk '{print $6}'`
-			LEFT_NODE_LSITS=`echo "$memlist" | grep -v "\<$SELF_HOST\>" | awk '{print $1}' | xargs echo | tr ' ' ','`
-			return 0
-		fi
+        if [[ "x$leader" != "x" ]]; then
+            QUERY_PORT=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $5}'`
+            EDIT_LOG_PORT=`echo "$memlist" | grep '\<LEADER\>' | awk '{print $3}'`
+            SELF_ROLE=`echo "$memlist" | grep "\<$SELF_HOST\>" | awk '{print $6}'`
+            LEFT_NODE_LSITS=`echo "$memlist" | grep -v "\<$SELF_HOST\>" | awk '{print $1}' | xargs echo | tr ' ' ','`
+            return 0
+        fi
 
-		sleep $PROBE_INTERVAL
+        sleep $PROBE_INTERVAL
 
-	done
+    done
 }
 
 transfer_master()
 {
 
-	`java -jar $STARROCK_HOME/fe/lib/je-7.3.7.jar DbGroupAdmin -helperHosts $SELF_HOST:$EDIT_LOG_PORT -groupName PALO_JOURNAL_GROUP -transferMaster $LEFT_NODE_LSITS 30`
+    `java -jar $STARROCK_HOME/fe/lib/je-7.3.7.jar DbGroupAdmin -helperHosts $SELF_HOST:$EDIT_LOG_PORT -groupName PALO_JOURNAL_GROUP -transferMaster $LEFT_NODE_LSITS 30`
 }
 
 
 set_self_not_leader()
 {
-	while true
-	do
-	assignment_variable
-	if [[ "x$SELF_ROLE" == "xLEADER" ]] ; then
-		transfer_master 
-	fi
+    while true
+    do
+    assignment_variable
+    if [[ "x$SELF_ROLE" == "xLEADER" ]] ; then
+        transfer_master 
+    fi
 
-		local now=`date +%s`
-		let "expire=start+timeout"
-		if [[ $expire -le $now ]] ; then 
-			if $has_member ; then
-				log_stderr "Timed out, abort!"
-				exit 1
-			else 
-				log_stderrr "Timed out, no members detected ever, assume myself is the first node .."
-			fi
-		fi
-		sleep $PROBE_INTERVAL
+        local now=`date +%s`
+        let "expire=start+timeout"
+        if [[ $expire -le $now ]] ; then 
+                log_stderr "Timed out, abort!"
+                exit 1
+            fi
+        fi
+        sleep $PROBE_INTERVAL
 
-	done
+    done
 }
 
 stop_follower_observer()
 {
-		timeout 30 mysql --connect-timeout 2 -h $FE_LEADER -P $QUERY_PORT -u root --skip-column-names --batch -e "ALTER SYSTEM DROP follower '$SELF_HOST:$EDIT_LOG_PORT';"	
-			$STARROCK_HOME/fe/bin/stop_fe.sh
+        timeout 30 mysql --connect-timeout 2 -h $FE_LEADER -P $QUERY_PORT -u root --skip-column-names --batch -e "ALTER SYSTEM DROP follower '$SELF_HOST:$EDIT_LOG_PORT';"    
+            $STARROCK_HOME/fe/bin/stop_fe.sh
 }
 
 
