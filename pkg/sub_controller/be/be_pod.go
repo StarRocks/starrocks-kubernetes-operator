aef8eae4902c3f3ea204296a4d8dd5243938a77c
dxheming <dxheming@163.com>
2023-02-20 17:22:16 +0800

    fix be pod error log-storage-name and annotations (#45)
    * fix be pod error log-storage-name and annotations
    
    Signed-off-by: haiming <dxheming@163.com>

--- pkg/sub_controller/be/be_pod.go
+++ pkg/sub_controller/be/be_pod.go
@@ -79,6 +79,7 @@ func (be *BeController) buildPodTemplate(src *srapi.StarRocksCluster, beconfig m
 			Name:      log_name,
 			MountPath: log_path,
 		})
+
 		vols = append(vols, corev1.Volume{
 			Name: log_name,
 			VolumeSource: corev1.VolumeSource{
