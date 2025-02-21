916e90bd55981f2f1bed748bfa0bb9cf1fa1d013
yandongxiao <yandongxiao@starrocks.com>
2025-02-21 14:09:08 +0800

    [Documentation] prepare for v1.10.0 (#624)
    Signed-off-by: yandongxiao <yandongxiao@starrocks.com>

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# You should use it like this: bash ./scripts/change-version.sh 1.9.8 1.9.9
+# You should use it like this: bash ./scripts/change-version.sh 1.9.8 1.10 1.10.09.9
 
 find . -type f | while read -r file; do
   # ignore files or directories starting with a dot
